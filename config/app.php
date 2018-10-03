d55c3d62fac2d7f0cefddb2401a99de8cb3e0174
brankaDakic10 <branka.dakic@yahoo.com>
2018-10-03 15:28:44 +0200

    5.Task-implemented b cors in project

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Barryvdh\Cors\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
